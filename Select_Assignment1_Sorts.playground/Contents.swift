d56e8be580b0b06ed7b9bed0a514567ba8169a03
Hieu Vo <vpmhieu@gmail.com>
2016-06-26 23:17:01 +0700

    implement selectSort and quickSort

--- Select_Assignment1_Sorts.playground/Contents.swift
+++ Select_Assignment1_Sorts.playground/Contents.swift
@@ -32,9 +32,83 @@ public class SortTestCase : XCTestCase {
         let list = [3, 2, 1]
         XCTAssertEqual(sortMethod(list), [1, 2, 3])
     }
+    
+    func testListWithDuplicate() {
+        let list = [1, 2, 1]
+        XCTAssertEqual(sortMethod(list), [1, 1, 2])
+    }
 }
 
-SortTestCase(method: bogoSort)
+func selectSort(list: [Int]) -> [Int] {
+    print ("Start SelectSort")
+    if list.count < 2 {
+        return list
+    }
+    
+    var newList = list
+    
+    for i in 0..<(newList.count-1) {
+        var k = i
+        
+        for j in (i+1)..<newList.count {
+            if newList[k] > newList[j] {
+                k = j
+            }
+        }
+        
+        if k != i {
+            let t = newList[i]
+            newList[i] = newList[k]
+            newList[k] = t
+        }
+    }
+    
+    return newList
+}
+
+func quickSortNew(list: [Int]) -> [Int] {
+    if list.count < 2 {
+        return list
+    }
+    
+    return quickSortOperate(list, start: 0, end: (list.count - 1))
+}
+
+func quickSortOperate(list: [Int], start: Int, end: Int) -> [Int] {
+    if start >= end {
+        return list
+    }
+    
+    var newList = list
+    let pivotValue = newList[start]
+    var left = start - 1
+    var right = end + 1
+    
+    while (left < right) {
+        repeat {
+            left += 1
+        } while newList[left] < pivotValue
+        
+        repeat {
+            right -= 1
+        } while pivotValue < newList[right]
+        
+        if left >= right {
+            break
+        }
+        
+        let t = newList[left]
+        newList[left] = newList[right]
+        newList[right] = t
+    }
+    
+    newList = quickSortOperate(newList, start: start, end: right)
+    newList = quickSortOperate(newList, start: right+1 , end: end)
+    
+    return newList
+}
+
+//SortTestCase(method: bogoSort)
 //SortTestCase(method: selectSort)
-//SortTestCase(method: quickSort)
+//SortTestCase(method: quickSortNew)
 //SortTestCase(method: mergeSort)
